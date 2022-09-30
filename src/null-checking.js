aaac902f6c65fe12417c1d72865ea15651dff15e
Hamza Ashraf <hamzamirza658@gmail.com>
2022-09-30 19:44:58 +0500

    phone number

---
+++ src/null-checking.js
@@ -0,0 +1,20 @@
+const isNull = (value) => {
+  if (
+    value === '' ||
+    value === 0 ||
+    value === 'Null' ||
+    value === null ||
+    value === undefined ||
+    value === 'Please Select' ||
+    value.length === 0 ||
+    value === -1 ||
+    value === 'Please Coverage' ||
+    value === 'Please Relationship'
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+export default isNull
